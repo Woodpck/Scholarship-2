9029777f2ea1d9fc25bd0329167b84907501f15c
polykrystenum <thatrealjerobrine@gmail.com>
2024-05-27 01:38:25 +0800

    field names changed.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,35 +23,51 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
+    {   
+        //random full name generator
+        $firstName = $this->faker->firstName;
+        $middleName = $this->faker->lastName;
+        $lastName = $this->faker->lastName;
+        $name = $firstName . ' ' . $middleName . ' ' . $lastName;
+
+        //random plm email generator
+        $studentNo = $this->faker->numerify('20##-#####');
+        $firstInitial = substr($firstName, 0, 1);
+        $middleInitial = substr($middleName, 0, 1);
+        $plmEmail = strtolower($firstInitial . $middleInitial . $lastName . substr($studentNo, 0, 4) . '@plm.edu.ph');
+        $username = strtolower($firstInitial . $middleInitial . $lastName);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'phone' => fake()->phoneNumber,
-            'address' => fake()->address,
-            'student_no' => fake()->randomNumber(),
-            'last_name' => fake()->lastName,
-            'first_name' => fake()->firstName,
-            'gender' => fake()->randomElement(['Male', 'Female']),
-            'age' => fake()->numberBetween(17, 60),
+            'student_no' => $studentNo,
+            'last_name' => $lastName,
+            'first_name' => $firstName,
+            'middle_name' => $middleName,
+            'biological_sex' => $this->faker->randomElement(['Male', 'Female']),
+            'age' => $this->faker->numberBetween(18, 30),
             'college' => fake()->word,
-            'course' => fake()->word,
-            'grade_file' => fake()->randomElement(['grade.pdf', 'grade.doc']), // Path or filename for grade file
-            'form_with_pic' => fake()->randomElement(['form_with_pic.jpg', 'gform_with_pic.png']), // Path or filename for form with picture
-            'gmc_cert' => fake()->randomElement(['gmc_cert.pdf', 'gmc_cert.doc']),
+            'degree_program_id' => $this->faker->numberBetween(1, 37),
+            'academic_year' => $this->faker->randomElement(['1st Year', '2nd Year', '3rd Year', '4th Year']),
+            'plm_email' => $plmEmail,
+            'mobile_no' => $this->faker->numerify('(+63)9## ### ####'),
+            'grades' => $this->faker->randomFloat(2, 1.00, 2.50),
+            'annual_family_income' => $this->faker->randomFloat(2, 10000, 200000),
+            'address' => fake()->address,
+            'grade_file' => fake()->randomElement(['grade.pdf', 'grade.doc']),
             'tax' => fake()->randomElement(['tax.pdf', 'tax.doc']),
-            'reason_letter' => fake()->randomElement(['reason_letter.pdf', 'reason_letter.doc']),
             'id_reg_form' => fake()->randomElement(['id_reg_form.pdf', 'id_reg_form.doc']),
-            'brg_cert' => fake()->randomElement(['brgy_cert.pdf', 'brgy_cert.doc']),
             'single_parent_id' => fake()->randomElement(['single_parent_id.pdf', 'single_parent_id.doc']),
-            'year_level' => fake()->numberBetween(1, 4),
-            'current_gwa' => fake()->randomFloat(2, 1, 5),
-            'household_income' => fake()->randomFloat(2, 100000, 1000000),
-            'photo' => fake()->imageUrl('60','60'),
-            'role' => fake()->randomElement(['admin','agent', 'opa', 'user',]),
-            'status' => fake()->randomElement(['active','inactive']),
+            'form_with_pic' => fake()->randomElement(['form_with_pic.jpg', 'gform_with_pic.png']),
+            'gmc_cert' => fake()->randomElement(['gmc_cert.pdf', 'gmc_cert.doc']),
+            'reason_letter' => fake()->randomElement(['reason_letter.pdf', 'reason_letter.doc']),
+            'brg_cert' => fake()->randomElement(['brgy_cert.pdf', 'brgy_cert.doc']),
+            'photo' => fake()->imageUrl('60', '60'),
+            'name' => $name,
+            'username' => $username,
+            'email' => $plmEmail,
+            'password' => static::$password ??= Hash::make('password'),
+            'email_verified_at' => now(),
+            'role' => fake()->randomElement(['admin', 'agent', 'opa', 'user',]),
+            'status' => fake()->randomElement(['active', 'inactive']),
             'remember_token' => Str::random(10),
         ];
     }
---
+++ database/migrations/2024_05_26_172053_create_users_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            //student information field names from the data dictionary
+            $table->string('student_no')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('biological_sex')->nullable();
+            $table->string('age')->nullable();
+            $table->string('college')->nullable();
+            $table->string('degree_program_id')->nullable();
+            $table->string('academic_year')->nullable();
+            $table->string('plm_email')->nullable();
+            $table->string('mobile_no')->nullable();
+            $table->decimal('grades', 4, 2)->nullable(); //formerly current_gwa
+            $table->decimal('annual_family_income', 10, 2)->nullable();
+            //student information field that is questionable
+            $table->string('address')->nullable();
+            //student files field names from the data dictionary
+            $table->string('grade_file')->nullable();
+            $table->string('tax')->nullable();
+            $table->string('id_reg_form')->nullable();
+            $table->string('single_parent_id')->nullable();
+            $table->string('form_with_pic')->nullable();
+            $table->string('gmc_cert')->nullable();
+            $table->string('reason_letter')->nullable();
+            $table->string('brg_cert')->nullable();
+            $table->string('photo')->nullable();
+            //fields responsible for users logging into the site
+            $table->id();
+            $table->string('name');
+            $table->string('username')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['admin','agent','opa','user','student',])->default('user');
+            $table->enum('status',['active','inactive'])->default('active');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
