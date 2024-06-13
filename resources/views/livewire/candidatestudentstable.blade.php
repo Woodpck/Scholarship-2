d3e1b017433b6793955ea7a16cb87a95dae2dd33
polykrystenum <thatrealjerobrine@gmail.com>
2024-06-13 16:07:15 +0800

    Update candidatestudentstable.blade.php

--- resources/views/livewire/candidatestudentstable.blade.php
+++ resources/views/livewire/candidatestudentstable.blade.php
@@ -11,10 +11,10 @@
                 </div>
             </div>
             <div class="overflow-x-auto flex flex-center">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 flex flex-center">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr></tr>
-                        <th scope="col" class="px-4 py-3 flex items-center">STUDENT NAME</th>
+                        <th scope="col" class="px-4 py-3">STUDENT NAME</th>
                         <th scope="col" class="px-4 py-3">STUDENT NUMBER</th>
                         <th scope="col" class="px-4 py-3">STATUS</th>
                         <th scope="col" class="px-4 py-3"> </th>
